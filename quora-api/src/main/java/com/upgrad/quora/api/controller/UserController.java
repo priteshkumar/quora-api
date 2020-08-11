c831d41b45c59e5724b097fcff0b68356f1354af
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-08-11 13:06:15 +0530

    add signin logic in Controller/Dao layer

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.SigninResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.AuthenticationService;
 import com.upgrad.quora.service.business.SignupBusinessService;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import java.util.Base64;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +25,9 @@ public class UserController {
 
   @Autowired
   SignupBusinessService signupBusinessService;
+
+  @Autowired
+  AuthenticationService authenticationService;
 
   @RequestMapping(method = RequestMethod.POST, path = "/user/signup", consumes =
       MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -52,5 +61,27 @@ public class UserController {
     SignupUserResponse signupUserResponse =
         new SignupUserResponse().id(createdUser.getUuid()).status("USER SUCCESSFULLY REGISTERED");
     return new ResponseEntity<>(signupUserResponse, HttpStatus.CREATED);
+  }
+
+
+  @RequestMapping(method = RequestMethod.POST, path = "/user/signin", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<SigninResponse> signin(
+      @RequestHeader("authorization") String authorization) {
+    String[] authData = authorization.split("Basic ");
+    System.out.println(authData[1]);
+    String[] loginDetails = new String(Base64.getDecoder().decode(authData[1])).split(":");
+
+    UserAuthEntity userAuthEntity = authenticationService.authenticate(loginDetails[0],
+        loginDetails[1]);
+
+    UserEntity userEntity = userAuthEntity.getUser();
+    SigninResponse signinResponse = new SigninResponse().id(userEntity.getUuid())
+        .message("SIGNED IN "
+            + "SUCCESSFULLY");
+
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.add("access-token", userAuthEntity.getAccessToken());
+    return new ResponseEntity<>(signinResponse, httpHeaders, HttpStatus.OK);
+
   }
 }
--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.upgrad.quora.service.dao;
 
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
@@ -12,26 +13,31 @@ public class UserDao {
   @PersistenceContext
   EntityManager entityManager;
 
-  public UserEntity createUser(UserEntity userEntity){
+  public UserEntity createUser(UserEntity userEntity) {
     entityManager.persist(userEntity);
     return userEntity;
   }
 
-  public UserEntity getUserByUserName(String userName){
-    try{
-      return entityManager.createNamedQuery("userByUserName",UserEntity.class).setParameter(
-          "userName",userName).getSingleResult();
-    }catch(NoResultException e){
+  public UserEntity getUserByUserName(String userName) {
+    try {
+      return entityManager.createNamedQuery("userByUserName", UserEntity.class).setParameter(
+          "userName", userName).getSingleResult();
+    } catch (NoResultException e) {
       return null;
     }
   }
 
-  public UserEntity getUserByEmail(String email){
-    try{
-      return entityManager.createNamedQuery("userByEmail",UserEntity.class).setParameter(
-          "email",email).getSingleResult();
-    }catch(NoResultException e){
+  public UserEntity getUserByEmail(String email) {
+    try {
+      return entityManager.createNamedQuery("userByEmail", UserEntity.class).setParameter(
+          "email", email).getSingleResult();
+    } catch (NoResultException e) {
       return null;
     }
+  }
+
+  public UserAuthEntity createUserAuth(UserAuthEntity userAuthEntity) {
+    entityManager.persist(userAuthEntity);
+    return userAuthEntity;
   }
 }
