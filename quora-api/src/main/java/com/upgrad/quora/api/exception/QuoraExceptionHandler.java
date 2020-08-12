492c81b169a5866b8e4ebbe620429dfd9d8c13b1
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-08-12 18:27:09 +0530

    add AuthorizationFailedException in handler

--- quora-api/src/main/java/com/upgrad/quora/api/exception/QuoraExceptionHandler.java
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/QuoraExceptionHandler.java
@@ -2,8 +2,10 @@ package com.upgrad.quora.api.exception;
 
 import com.upgrad.quora.api.model.ErrorResponse;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -26,7 +28,8 @@ public class QuoraExceptionHandler {
   public ResponseEntity<ErrorResponse> authenticationException(AuthenticationFailedException exe,
       WebRequest request) {
     return new ResponseEntity<ErrorResponse>(
-        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.UNAUTHORIZED
+        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()),
+        HttpStatus.UNAUTHORIZED
     );
   }
 
@@ -34,7 +37,25 @@ public class QuoraExceptionHandler {
   public ResponseEntity<ErrorResponse> signOutException(SignOutRestrictedException exe,
       WebRequest request) {
     return new ResponseEntity<ErrorResponse>(
-        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.UNAUTHORIZED
+        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()),
+        HttpStatus.UNAUTHORIZED
+    );
+  }
+
+  @ExceptionHandler(AuthorizationFailedException.class)
+  public ResponseEntity<ErrorResponse> authFailedException(AuthorizationFailedException exe,
+      WebRequest request) {
+    return new ResponseEntity<ErrorResponse>(
+        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()),
+        HttpStatus.FORBIDDEN
+    );
+  }
+
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<ErrorResponse> authFailedException(UserNotFoundException exe,
+      WebRequest request) {
+    return new ResponseEntity<ErrorResponse>(
+        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.NOT_FOUND
     );
   }
 }
