62917b941de085cb7f301632540205e771ed654c
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-08-13 18:47:57 +0530

    add getAllQuestions endpoint impl

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -5,6 +5,7 @@ import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -32,5 +33,17 @@ public class QuestionService {
     question.setUser(userAuth.getUser());
     QuestionEntity createdQuestion = questionDao.createQuestion(question);
     return createdQuestion;
+  }
+
+  public List<QuestionEntity> getAllQuestions(String accessToken)
+      throws AuthorizationFailedException {
+    UserAuthEntity userAuth = userDao.getUserAuth(accessToken);
+    if (userAuth == null) {
+      throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+    } else if (userAuth.getLogoutAt() != null) {
+      throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get"
+          + " all questions");
+    }
+    return questionDao.getAllQuestions();
   }
 }
