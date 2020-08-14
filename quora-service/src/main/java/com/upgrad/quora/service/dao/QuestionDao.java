9e05e7cebcc96bc8588c2b7c6963f9aa2f59006e
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-08-14 12:34:04 +0530

    add deleteQuestion endpoint impl

--- quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/QuestionDao.java
@@ -3,6 +3,7 @@ package com.upgrad.quora.service.dao;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
@@ -19,5 +20,20 @@ public class QuestionDao {
 
   public List<QuestionEntity> getAllQuestions() {
     return entityManager.createNamedQuery("allQuestions", QuestionEntity.class).getResultList();
+  }
+
+  public QuestionEntity getQuestionByUUID(String uuid) {
+    try {
+      return entityManager.createNamedQuery("questionByUuid", QuestionEntity.class).setParameter(
+          "uuid",
+          uuid).getSingleResult();
+    } catch (NoResultException e) {
+      return null;
+    }
+  }
+
+  public QuestionEntity deleteQuestion(QuestionEntity questionEntity) {
+    entityManager.remove(questionEntity);
+    return questionEntity;
   }
 }
