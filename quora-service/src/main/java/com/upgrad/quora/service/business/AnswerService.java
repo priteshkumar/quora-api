cd6668aa05684bf784fdb9e1fbb4359cae487e39
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-08-16 19:34:50 +0530

    add editAnswerContent endpoint impl
    rebase change on master

--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerService.java
@@ -92,4 +92,26 @@ public class AnswerService {
     List<AnswerEntity> answerList = answerDao.getAllAnswersByQuestion(question.getId());
     return answerList;
   }
+
+  @Transactional(propagation = Propagation.REQUIRED)
+  public AnswerEntity editAnswer(String accessToken, String content, String answerId)
+      throws AuthorizationFailedException, AnswerNotFoundException {
+    UserAuthEntity userAuth = userDao.getUserAuth(accessToken);
+    if (userAuth == null) {
+      throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+    } else if (userAuth.getLogoutAt() != null) {
+      throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to edit"
+          + " an answer");
+    }
+    AnswerEntity answer = answerDao.getAnswerByUUID(answerId);
+    if (answer == null) {
+      throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+    }
+    if (!answer.getUser().getUserName().equals(userAuth.getUser().getUserName())) {
+      throw new AuthorizationFailedException("ATHR-003", "Only the answer owner can edit the "
+          + "answer");
+    }
+    answer.setAns(content);
+    return answerDao.editAnswer(answer);
+  }
 }
