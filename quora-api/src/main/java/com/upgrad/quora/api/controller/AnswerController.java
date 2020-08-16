105dd869b8f610d3fe08dadaef48c9359a10a946
Pritesh Kumar <pritesh.kumar2@gmail.com>
2020-08-16 12:44:08 +0530

    add allAnswersToQuestion endpoint impl

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -1,6 +1,7 @@
 package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.AnswerDeleteResponse;
+import com.upgrad.quora.api.model.AnswerDetailsResponse;
 import com.upgrad.quora.api.model.AnswerRequest;
 import com.upgrad.quora.api.model.AnswerResponse;
 import com.upgrad.quora.service.business.AnswerService;
@@ -9,6 +10,8 @@ import com.upgrad.quora.service.entity.AnswerEntity;
 import com.upgrad.quora.service.exception.AnswerNotFoundException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -40,7 +43,7 @@ public class AnswerController {
     return new ResponseEntity<>(answerResponse, HttpStatus.CREATED);
   }
 
-  //deleteAnswer - "/answer/delete/{answerId}"
+
   @RequestMapping(method = RequestMethod.DELETE, path = "/answer/delete/{answerId}",
       produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<AnswerDeleteResponse> deleteAnswer(
@@ -53,5 +56,26 @@ public class AnswerController {
     answerDeleteResponse.setId(deletedAnswer.getUuid());
     answerDeleteResponse.setStatus("ANSWER DELETED");
     return new ResponseEntity<>(answerDeleteResponse, HttpStatus.OK);
+  }
+
+
+  @RequestMapping(method = RequestMethod.GET, path = "/answer/all/{questionId}",
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(
+      @PathVariable("questionId") String questionId,
+      @RequestHeader("authorization") String authorization)
+      throws AuthorizationFailedException, InvalidQuestionException {
+    String accessToken = AuthTokenParser.parseAuthToken(authorization);
+    List<AnswerEntity> aList = answerService
+        .getAllAnswersToQuestion(accessToken, questionId);
+    List<AnswerDetailsResponse> answerDetailsResponseList = new ArrayList<>();
+    aList.forEach((a) -> {
+      AnswerDetailsResponse answerDetailsResponse = new AnswerDetailsResponse();
+      answerDetailsResponse.setId(a.getUuid());
+      answerDetailsResponse.setQuestionContent(a.getQuestion().getContent());
+      answerDetailsResponse.setAnswerContent(a.getAns());
+      answerDetailsResponseList.add(answerDetailsResponse);
+    });
+    return new ResponseEntity<>(answerDetailsResponseList, HttpStatus.OK);
   }
 }
